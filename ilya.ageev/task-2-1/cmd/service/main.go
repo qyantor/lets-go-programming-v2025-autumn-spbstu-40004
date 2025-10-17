e6c3e763b9f5453c01d3318822fc94554d09533a
qyantor <ilyxaplay220@gmail.com>
2025-10-17 21:01:15 +0300

    [TASK-2-1] Initial commit

---
+++ ilya.ageev/task-2-1/cmd/service/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+  "fmt"
+  "strconv"
+  "strings"
+)
+
+func processDepartment(numOfWork int) []string {
+  const (
+    minAllowedTemp = 15
+    maxAllowedTemp = 30
+  )
+
+  minT := 15
+  maxT := 30
+  departmentResults := make([]string, 0, numOfWork)
+
+  for range numOfWork {
+    var str string
+
+    var Temp int
+
+    _, err := fmt.Scan(&str, &Temp)
+    if err != nil  Temp > maxAllowedTemp  Temp < minAllowedTemp {
+      fmt.Println("Invalid temperature")
+
+      return nil
+    }
+
+    if str == ">=" {
+      if Temp > minT {
+        minT = Temp
+      }
+    } else if str == "<=" {
+      if Temp < maxT {
+        maxT = Temp
+      }
+    }
+
+    if minT <= maxT {
+      departmentResults = append(departmentResults, strconv.Itoa(minT))
+    } else {
+      departmentResults = append(departmentResults, "-1")
+    }
+  }
+
+  return departmentResults
+}
+
+func main() {
+  var numOfDepart int
+
+  _, err := fmt.Scan(&numOfDepart)
+  if err != nil {
+    fmt.Println("Invalid number of departments")
+
+    return
+  }
+
+  results := make([]string, 0)
+
+  for range numOfDepart {
+    var numOfWork int
+
+    _, err := fmt.Scan(&numOfWork)
+    if err != nil {
+      fmt.Println("invalid number of workers")
+
+      return
+    }
+
+    departmentResults := processDepartment(numOfWork)
+    if departmentResults == nil {
+      return
+    }
+
+    results = append(results, departmentResults...)
+  }
+
+  fmt.Println(strings.Join(results, "\n"))
+}
---
+++ ilya.ageev/task-2-1/go.mod
@@ -0,0 +1,3 @@
+module github.com/qyantor/task-2-1
+
+go 1.22.7
