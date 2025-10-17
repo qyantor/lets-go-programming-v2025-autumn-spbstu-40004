4869d9e9410835eb506320990b271c8089cd63a6
qyantor <ilyxaplay220@gmail.com>
2025-10-17 21:46:55 +0300

    [TASK-2-2] fixed lint

--- ilya.ageev/task-2-2/cmd/service/main.go
+++ ilya.ageev/task-2-2/cmd/service/main.go
@@ -7,47 +7,57 @@ import (
 
 type IntHeap []int
 
-func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
-func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (DishHeap IntHeap) Len() int           { return len(DishHeap) }
+func (DishHeap IntHeap) Less(i, j int) bool { return DishHeap[i] < DishHeap[j] }
+func (DishHeap IntHeap) Swap(i, j int)      { DishHeap[i], DishHeap[j] = DishHeap[j], DishHeap[i] }
 
-func (h *IntHeap) Push(x interface{}) {
-	*h = append(*h, x.(int))
+func (DishHeap *IntHeap) Push(x interface{}) {
+	*DishHeap = append(*DishHeap, x.(int))
 }
 
-func (h *IntHeap) Pop() interface{} {
-	old := *h
+func (DishHeap *IntHeap) Pop() interface{} {
+	old := *DishHeap
 	n := len(old)
 	x := old[n-1]
-	*h = old[0 : n-1]
+	*DishHeap = old[0 : n-1]
 	return x
 }
 
 func main() {
-	var N, k int
-	fmt.Scan(&N)
+	var NumOfDishes, Priority int
 
-	arr := make([]int, N)
-	for i := 0; i < N; i++ {
-		fmt.Scan(&arr[i])
+	_, err := fmt.Scan(&NumOfDishes)
+
+	if err != nil {
+		fmt.Println("Invalid number of dishes")
+		return
 	}
 
-	fmt.Scan(&k)
-
-	h := &IntHeap{}
-	heap.Init(h)
-
-	for i := 0; i < k; i++ {
-		heap.Push(h, arr[i])
-	}
-
-	for i := k; i < N; i++ {
-		if arr[i] > (*h)[0] {
-			heap.Pop(h)
-			heap.Push(h, arr[i])
+	arr := make([]int, NumOfDishes)
+	for i := range arr {
+		_, err := fmt.Scan(&arr[i])
+		if err != nil {
+			fmt.Println("Invalid priority")
+			return
 		}
 	}
 
-	result := (*h)[0]
+	fmt.Scan(&Priority)
+
+	DishHeap := &IntHeap{}
+	heap.Init(DishHeap)
+
+	for _, num := range arr[:Priority] {
+		heap.Push(DishHeap, num)
+	}
+
+	for _, num := range arr[Priority:] {
+		if num > (*DishHeap)[0] {
+			heap.Pop(DishHeap)
+			heap.Push(DishHeap, num)
+		}
+	}
+
+	result := (*DishHeap)[0]
 	fmt.Println(result)
 }
